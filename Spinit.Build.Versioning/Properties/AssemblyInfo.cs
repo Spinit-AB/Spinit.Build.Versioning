2fe1434cbedf63625b6b4e17856f70e5aedb3ff6
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2019-02-20 16:34:06 +0100

    imported from hg and fixed RunOctoPack?

---
+++ Spinit.Build.Versioning/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Spinit.Build.Versioning")]
+[assembly: AssemblyProduct("Spinit.Build")]
+[assembly: AssemblyCompany("Spinit AB")]
+[assembly: AssemblyCopyright("Copyright © Spinit AB 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyDescription("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0fcb660-149a-4698-a65e-9c451078a70e")]
+
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyInformationalVersion("2.0.1.0")]
---
+++ Spinit.Build.Versioning/install.ps1
@@ -0,0 +1,2 @@
+﻿param($installPath, $toolsPath, $package, $project)
+$project.Object.References | Where-Object { $_.Name -eq 'Spinit.Build.Versioning' } | ForEach-Object { $_.Remove() }
